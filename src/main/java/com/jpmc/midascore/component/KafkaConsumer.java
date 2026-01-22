a40e795562086b76eb6425011cd794e8d5d0b925
Praveen kumar S <praveen0031kumar@gmail.com>
2026-01-22 20:48:52 +0530

    kafka integrated

---
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.jpmc.midascore.component;
+
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(
+            topics = "${general.kafka-topic}",
+            groupId = "midas-core-group"
+    )
+    public void consume(Transaction transaction) {
+
+    }
+}
